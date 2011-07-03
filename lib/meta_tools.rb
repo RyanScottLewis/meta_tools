3f545c674aa3bce2435bc1afd7a14586c809dadf
Ryan Scott Lewis <c00lryguy@gmail.com>
2011-07-03 03:29:49 -0400

    deprecate #metatools

--- lib/meta_tools.rb
+++ lib/meta_tools.rb
@@ -1,15 +1,20 @@
 module MetaTools
   # The hidden singleton lurks behind everyone
   def metaclass
+    puts "Warning: #metaclass will be deprecated in 0.3.0. Use #meta_class"
+    meta_class
+  end
+  
+  def meta_class
     class << self; self; end
   end
   
-  # Evaluate a block within the instance of the metaclass
+  # Evaluate a block within the instance of the meta_class
   def meta_eval(&blk)
-    metaclass.instance_eval(&blk)
+    meta_class.instance_eval(&blk)
   end
 
-  # Adds methods to a metaclass
+  # Adds methods to a meta_class
   def meta_def(name, &blk)
     meta_eval { define_method(name, &blk) }
   end
@@ -19,9 +24,9 @@ module MetaTools
     class_eval { define_method(name, &blk) }
   end
   
-  # Send a method to the metaclass
+  # Send a method to the meta_class
   def meta_send(meth, *args, &blk)
-    metaclass.send(meth, *args, &blk)
+    meta_class.send(meth, *args, &blk)
   end
   
   # Send a method to the class
