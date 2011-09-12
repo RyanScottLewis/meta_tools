0756930570a7bb08a3acc6eb903f8e61e98b5558
Ryan Scott Lewis <c00lryguy@gmail.com>
2011-09-11 21:55:18 -0400

    Adds meta_exec, meta_methods, and meta_send

--- lib/meta_tools.rb
+++ lib/meta_tools.rb
@@ -1,7 +1,7 @@
 module MetaTools
   # The hidden singleton lurks behind everyone
   def metaclass
-    puts "Warning: #metaclass will be deprecated in 0.3.0. Use #meta_class"
+    warn "[DEPRECATION] `metaclass` will be deprecated in 0.3.0. Please use `meta_class` instead."
     meta_class
   end
   
@@ -13,20 +13,30 @@ module MetaTools
   def meta_eval(&blk)
     meta_class.instance_eval(&blk)
   end
-
-  # Adds methods to a meta_class
-  def meta_def(name, &blk)
-    meta_eval { define_method(name, &blk) }
-  end
-
-  # Defines an instance method within a class
-  def class_def(name, &blk)
-    class_eval { define_method(name, &blk) }
+  
+  # Evaluate a block within the instance of the meta_class
+  def meta_exec(*args, &blk)
+    meta_class.instance_exec(*args, &blk)
   end
   
   # Send a method to the meta_class
   def meta_send(meth, *args, &blk)
     meta_class.send(meth, *args, &blk)
+  end
+  
+  # Adds methods to a meta_class
+  def meta_def(name, &blk)
+    meta_eval { define_method(name, &blk) }
+  end
+  
+  # Returns an Array of methods defined only within this Object.
+  def meta_methods
+    self.class.instance_methods - Object.instance_methods
+  end
+  
+  # Defines an instance method within a class
+  def class_def(name, &blk)
+    class_eval { define_method(name, &blk) }
   end
   
   # Send a method to the class
